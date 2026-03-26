bff60c872036222466584845028e8e27ad9853cc
Kayla Rampersaud <krampersaud98@gmail.com>
2026-03-26 10:34:46 -0400

    fixed merge conflict

--- api/index.js
+++ api/index.js
@@ -7,9 +7,11 @@ import dailyTotalsRouter from "#api/dailyTotals";
 import ingredientsRouter from "#api/ingredients";
 import mealIngredientsRouter from "#api/mealIngredients";
 import mealsRouter from "#api/meals";
+import savedMealsRouter from "#api/savedMeals";
 
 router.use("/users", usersRouter);
 router.use("/dailyTotals", dailyTotalsRouter);
 router.use("/ingredients", ingredientsRouter);
 router.use("/mealIngredients", mealIngredientsRouter);
 router.use("/meals", mealsRouter);
+router.use("/savedMeals", savedMealsRouter);
